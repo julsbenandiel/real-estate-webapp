aa4f8405bb8cd960618d1407bde6f2b8a9800b86
Juls-07 <junell.jacinto@hoppler.com.ph>
2021-04-26 00:22:20 +0800

    fix homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,9 @@ import Head from 'next/head'
 import { contentfulClient } from './contentful';
 import { PropertyModel } from './model/property.model';
 import PropertyCard from './components/PropertyCard';
+import PropertyCardBare from './components/PropertyCardBare';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouseUser } from '@fortawesome/free-solid-svg-icons';
 
 export async function getStaticProps() {
 
@@ -23,27 +26,53 @@ export default function Home({ total, properties }) {
   console.log({ total, properties });
   
   return (
-    <div className="max-w-screen-xl mx-auto mt-10">
+    <div className="max-w-screen-lg mx-auto mt-10">
 
       <p className="text-gray border border-lightGray py-20 text-center font-bold w-full rounded-lg">Banner</p>
       <p className="text-gray border border-lightGray py-10 text-center font-bold w-full rounded-lg mt-10">Section</p>
-    
-      <p className="text-gray font-semibold talic text-4xl mt-10">Featured Properties</p>
-      <br />
-      <div className="grid grid-cols-12 gap-5">
-        { properties.map((property: any) => 
-          <div
-            key={ property.sys.id } 
-            className="col-span-4">
-            <PropertyCard
-              title={ property.fields.title }
-              address={ property.fields.address }
-              description={ property.fields.shortDescription }
-              thumbnail={ property.fields.thumbnail.fields.file.url }
-            />
+
+      <section className="what-we-do mt-10">
+        <div className="grid grid-cols-12">
+          <div className="col-span-10">
+            <p className="text-gray nunito-sans tracking-widest text-sm font-medium uppercase">what we do</p>
+            <p className="playfair text-4xl mb-1 text-black">Helping you find the <span className="italic">property</span> of your dreams</p>
+            <p className="italic text-4xl text-black playfair">Creating real value in property and places.</p>
           </div>
-        )}
-      </div>
+          <div className="col-span-2 h-full flex items-center">
+            <button className="w-full px-5 py-3 text-sm mt-2 text-gray border border-lightBlack transition duration-150 hover:bg-black hover:text-white">
+              See all listings
+            </button>
+          </div>
+        </div>
+
+        <hr 
+          style={{ height: "1.4px" }}
+          className="bg-black mt-5"/>
+
+        <br />
+        <div className="grid grid-cols-12 gap-5">
+          { properties.map((property: any) => 
+            <div
+              key={ property.sys.id } 
+              className="col-span-4">
+              <PropertyCardBare
+                title={ property.fields.title }
+                address={ property.fields.address }
+                description={ property.fields.shortDescription }
+                thumbnail={ property.fields.thumbnail.fields.file.url }
+                bedroom={ 2 }
+                floorArea={ 210 }
+                bathroom={ 1 }
+                price={ property.fields.price }
+                forSale={ property.fields.forSale }
+                forRent={ property.fields.forRent }
+                imageCount={ property.fields.images.length }
+              />
+            </div>
+          )}
+        </div>
+      </section>
     </div>
   )
 }
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,16 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
+      white: "#ffffff",
+      black: "#171717",
       transparent: 'transparent',
       green: '#10B981',
       darkGreen: '#047857',
       gray: '#4B5563',
       lightGray: '#E5E7EB',
-      blue: '##2563EB'
+      lightBlack: "#696969",
+      blue: '#2563EB',
+      orange: '#f2922c'
     }
   },
   variants: {
